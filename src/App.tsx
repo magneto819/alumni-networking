4cb450e9ed4ab305eebbfc86cee64797331fd97c
magneto819 <magneto.zhao@gmail.com>
2026-02-15 23:24:00 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="*" element={<div className="p-8 text-center"><h1 className="text-2xl">页面未找到</h1></div>} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
